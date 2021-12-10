ea7e902986ff5fcbf6734c7da1d0fc901354dfb9
wujieliu <liuliuwujie@126.com>
2021-12-10 09:58:03 +0800

    添加影片实体

--- ABookSpider.ConsoleApp/EntitySpider.cs
+++ ABookSpider.ConsoleApp/EntitySpider.cs
@@ -7,8 +7,10 @@ using DotnetSpider.Downloader;
 using DotnetSpider.Http;
 using DotnetSpider.Infrastructure;
 using DotnetSpider.MySql;
+using DotnetSpider.MySql.Scheduler;
 using DotnetSpider.Scheduler;
 using DotnetSpider.Scheduler.Component;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
@@ -48,6 +50,8 @@ namespace ABookSpider.ConsoleApp
         public EntitySpider(IOptions<SpiderOptions> options, DependenceServices services, ILogger<Spider> logger) : base(options, services, logger)
         {
         }
+
+
         
 
         protected override async Task InitializeAsync(CancellationToken stoppingToken = default)
@@ -55,20 +59,23 @@ namespace ABookSpider.ConsoleApp
             AddDataFlow(GetDefaultStorage());
             AddDataFlow(new StarsParser());
             AddDataFlow(new ImageStorage());
-            AddDataFlow(new DataParser<StarsEntity>());
+            AddDataFlow(new DataParser<MovieEntity>());
             //AddDataFlow(new GenreParser());
 
             AddDataFlow(GetDefaultStorage());
-            for (int i = 0; i < 206; i++)
-            {
-                await AddRequestsAsync(
-                              new Request(
-                                  "https://avmoo.casa/cn/actresses/page/" + (i + 1),new Dictionary<string, object>{ { "page", i + 1 } }));
-            }
-          
+            //for (int i = 0; i < 206; i++)
+            //{
+            //    await AddRequestsAsync(
+            //                  new Request(
+            //                      "https://avmoo.casa/cn/actresses/page/" + (i + 1),new Dictionary<string, object>{ { "page", i + 1 } }));
+            //}
+
             //await AddRequestsAsync(
             //    new Request(
-            //        " https://avmoo.casa/cn/genre"));
+            //        " https://avmoo.casa/cn/genre")); 
+            await AddRequestsAsync(
+           new Request(
+               " https://avmoo.casa/cn/movie/a5ec7dc9ad49895c"));
 
         }
 
---
+++ ABookSpider.Entity/MovieEntity.cs
@@ -0,0 +1,73 @@
+﻿using DotnetSpider.DataFlow.Parser;
+using DotnetSpider.DataFlow.Storage;
+using DotnetSpider.Selector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ABookSpider.Entity
+{
+    [Schema("abook", "a_movie")]
+    [EntitySelector(Expression = ".//div[@class='container']", Type = SelectorType.XPath)]
+    [FollowRequestSelector(Expressions = new[] { "cn/movie/']" }, SelectorType = SelectorType.Regex)]
+    public class MovieEntity : EntityBase<MovieEntity>
+    {
+        protected override void Configure()
+        {
+            HasKey(x => x.LinkId);
+        }
+
+        [Required]
+        [StringLength(40)]
+        [Column("link_id")]
+        [ValueSelector(Expression = "linkId", Type = SelectorType.Environment)]
+        public string LinkId { get; set; }
+
+        //[StringLength(100)]
+        //[ValueSelector(Expression = "name", Type = SelectorType.Environment)]
+        [Column("name")]
+        [ValueSelector(Expression = ".//h3")]
+        public string Name { get; set; }
+        public string BigImage { get; set; }
+
+        public string A_ID { get; set; }
+
+
+        public string release_date { get; set; }
+
+        public string length { get; set; }
+
+        public string director { get; set; }
+
+        public string director_link_id { get; set; }
+
+        public string studio { get; set; }
+
+        public string studio_link_id { get; set; }
+
+        public string label { get; set; }
+
+        public string label_link_id { get; set; }
+
+        public string image_len { get; set; }
+
+        public string series { get; set; }
+
+        public string series_link_id { get; set; }
+
+
+        public string genres { get; set; }
+
+        public string stars { get; set; }
+
+
+
+
+
+
+    }
+}
